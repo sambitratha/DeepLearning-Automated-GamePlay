da5e5905cb215abeaf9636afd4f6611abb9b2221
Sambit Rath <es15btech11015@iith.ac.in>
2018-03-30 19:44:37 +0530

    Update Car.py

--- Car.py
+++ Car.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pygame
+import random
 from pygame.locals import *
 import pygame.locals as locals
 
@@ -33,7 +34,7 @@ print(welcomebg)
 
 def showWelcomeScreen():
     #show this screen when the user first starts the program
-    display = pygame.display.set_mode((800, 600))
+    display = pygame.display.set_mode((windowlength, windowheight))
     pygame.display.set_caption("Car Racing")
     print(welcomebg.get_rect())
     while True:
@@ -53,6 +54,55 @@ def showWelcomeScreen():
 
 def maingame():
     #execute this when user starts playing
+    ballx = windowlength/4
+    bally = (windowheight - upmargin) / 2
+    radius = 5
+    width = 50
+    gap = 20
+    screenheight = windowheight - upmargin
+    distance = windowlength/2
+    pipes = []
+    #add two pipes in the start of the game
+    pipes.append([windowlength/2,random.randint(10, screenheight - 10)])
+    pipes.append([windowlength, random.randint(10, screenheight - 10)])
+
+    velx = -4
+    clock = pygame.time.Clock()
+    display = pygame.display.set_mode((windowlength, windowheight))
+    pygame.display.set_caption("Car Racing")
+    while True:
+        display.fill(colors['white'])
+
+        pygame.draw.circle(display, colors['red'], (ballx, bally), radius)
+
+        for pipe in pipes:
+            pygame.draw.rect(display, colors['black'], (pipe[0], upmargin, width, pipe[1]))
+            lpipex = pipe[0]
+            lpipey = upmargin + pipe[1] + gap
+            lpipewidth = width
+            lpipeheight = screenheight - pipe[1] - gap
+            pygame.draw.rect(display, colors['black'], (lpipex, lpipey, lpipewidth, lpipeheight))
+
+        for pipe in pipes:
+            pipe[0] += velx
+
+        if pipes[-1][0] + distance < windowlength:
+            pipes.append([pipes[-1][0] + distance, random.randint(10, screenheight -10)])
+
+        if pipes[0][0] + pipes[0][1] < 0:
+            pipes.pop(0)
+
+
+        clock.tick(30)
+
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                pygame.quit()
+                return
+            
+
+        pygame.display.update()
+
     pass
 
 def showGameover():
