d06713a513179403a72ad232f5171a0b2800cc78
rahulag1997 <rahulag1997@gmail.com>
2018-04-11 15:07:38 +0530

    Coolision and Score

--- Car.py
+++ Car.py
@@ -3,6 +3,7 @@ import os
 import pygame
 import random
 from pygame.locals import *
+from  math import  cos, sin, pi
 import pygame.locals as locals
 
 
@@ -31,8 +32,10 @@ windowlength = 800
 
 upmargin = windowheight / 20
 
+font = None
 
-welcomebg = pygame.image.load('assets/welcome.png')
+
+welcomebg = pygame.image.load('assets/welcome2.jpg')
 print(welcomebg)
 
 #########################################################################################
@@ -43,7 +46,9 @@ def showWelcomeScreen():
     print(welcomebg.get_rect())
     while True:
         #display.fill(colors['white'])
-        display.blit(welcomebg, welcomebg.get_rect())
+        cropRect = (0, 0, windowlength, windowheight)
+        display.blit(welcomebg, dest = (0, 0), area= cropRect)
+        #display.blit(welcomebg, welcomebg.get_rect())
         pygame.display.update()
 
         for event in pygame.event.get():
@@ -58,11 +63,12 @@ def showWelcomeScreen():
 
 def maingame():
     #execute this when user starts playing
+    playerscore = 0
     pipeimage = pygame.image.load("assets/brick4.png")
     ballx = windowlength/4
     bally = (windowheight - upmargin) / 2
     radius = 15
-    width =80
+    width = 80
     gap = 100
     screenheight = windowheight - upmargin
     distance = windowlength/2
@@ -71,19 +77,46 @@ def maingame():
     pipes.append([windowlength/2,random.randint(50, screenheight - 50)])
     pipes.append([windowlength, random.randint(50, screenheight - 50)])
 
+
+    crossedPole = False
+
+    playerUpAccl = -10
+    playerMaxVel = 10
+    playerAccl = 1
     vely = 10
     velx = -10
     clock = pygame.time.Clock()
     display = pygame.display.set_mode((windowlength, windowheight))
     pygame.display.set_caption("Car Racing")
     up = False
-
+    flag = False
 
 
     while True:
         display.fill(colors['blue'])
+        curpipe = pipes[0]
+        if not crossedPole:
+            if ballx > curpipe[0] + width/2 :
+                playerscore += 1
+                crossedPole = True
+
+        ball = {'center' : (ballx, bally) , 'radius' : radius}
+        for pipe in pipes:
+            upipe = {}
+            upipe['start'] = [pipe[0], upmargin]
+            upipe['width'] = width
+            upipe['height'] = pipe[1]
+            lpipe = {}
+            lpipe['start'] = [pipe[0], upmargin + pipe[1] + gap]
+            lpipe['width'] = width
+            lpipe['height'] = screenheight - pipe[1] - gap
+
+            if checkForCollision(ball, upipe) or checkForCollision(ball, lpipe):
+               return
 
         pygame.draw.circle(display, colors['red'], (ballx, bally), radius)
+
+
 
         for pipe in pipes:
             #pygame.draw.rect(display, colors['black'], (pipe[0], upmargin, width, pipe[1]))
@@ -108,6 +141,8 @@ def maingame():
 
         #pop first pipe if needed
         if pipes[0][0] + pipes[0][1] < 0:
+            if  crossedPole:
+                crossedPole = False
             pipes.pop(0)
 
 
@@ -123,34 +158,85 @@ def maingame():
             #if player moved up then set the boolean field to true
             if event.type == KEYDOWN and event.key == K_UP:
                 up = True
+                vely = playerUpAccl
 
+
+        if vely < playerMaxVel and not up:
+            vely += playerAccl
         #if player pressed up then accelerate the ball upward
         if up:
-            vely = -10
             up = False
-
-        #slowly reduce speed of the ball
-        if vely < 10:
-            vely += 1
 
         #update the position of ball
         bally += vely
 
 
+
+
         pygame.draw.rect(display, (50, 50, 70, 100), (0, 0, windowlength, upmargin))
-        pygame.display.update()
+
+        tObject, tRect = getStringObject(str(playerscore), windowlength/2, 10)
+        display.blit(tObject, tRect)
+        if not flag:
+            pygame.display.update()
 
     pass
 
 def showGameover():
     #show this screen when the user is done playing the game
+
+    display = pygame.display.set_mode((windowlength, windowheight))
+    pygame.display.set_caption("Game Over")
+    gameoverBG = pygame.image.load('assets/gameover.jpg')
+    print(welcomebg.get_rect())
+    while True:
+        #display.fill(colors['white'])
+        cropRect = (0, 0, windowlength, windowheight)
+        display.blit(gameoverBG, dest = (0, 0), area= cropRect)
+
+        #tObject, tRect = getStringObject(str(playerscore), windowlength/2, 10)
+        #display.blit(tObject, tRect)
+        #display.blit(welcomebg, welcomebg.get_rect())
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+
+            if (event.type == KEYDOWN or event.type == KEYUP) and event.key == K_p:
+                return
     pass
 
 
 
+def checkForCollision(circle, rect):
+    (cx, cy) = circle['center']
+    r = circle['radius']
+    for degree in range(0, 360, 5):
+        theta = float(degree * pi) / 180
+        point = [r * cos(theta) + cx, r * sin(theta) + cy]
+        if rect['start'][0] < point[0] < rect['start'][0] + rect['width'] and rect['start'][1] < point[1] < rect['start'][1] + rect['height']:
+            return True
+
+    return False
+
+
+def getStringObject(s,centerx,centery):
+    color = colors['white']
+    textObj = font.render(s,True, color , (50, 50, 70))
+    textRect = textObj.get_rect()
+    textRect.center = (centerx, centery)
+    return textObj , textRect
+
+
+
 if __name__ == '__main__':
-    showWelcomeScreen()
-    maingame()
-    showGameover()
+    pygame.init()
+    font = pygame.font.Font("assets/EraserRegular.ttf",24)
+    for i in range(5):
+        showWelcomeScreen()
+        maingame()
+        showGameover()
 
 
